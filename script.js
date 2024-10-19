e3fb2954082864fca29117534958a9c7cebf08ce
jynxzio5 <motassemhwarat@gmail.com>
2024-10-20 00:16:59 +0300

    update v 1.1
    - added background music
    - tittle name changed

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// إضافة حدث عند الضغط على زر "نعم"
+let isPlaying = false; // لتتبع ما إذا كانت الموسيقى تعمل
+
 document.getElementById('yes').addEventListener('click', function() {
     // إعادة توجيه المستخدم إلى حساب إنستغرام
     window.location.href = 'https://www.instagram.com/8kp.m/';
 });
 
-// إضافة حدث عند الضغط على زر "لا"
 document.getElementById('no').addEventListener('click', function() {
-    const noButton = document.getElementById('no');
+    const audio = document.getElementById('backgroundMusic');
+    
+    // إذا لم تكن الموسيقى تعمل، شغلها
+    if (!isPlaying) {
+        audio.play(); // تشغيل الموسيقى
+        isPlaying = true; // تعيين الحالة إلى التشغيل
+    }
+    
     // تحريك الزر "لا" بعيدًا
+    const noButton = document.getElementById('no');
     noButton.style.transform = 'translate(' + (Math.random() * 200 - 100) + 'px,' + (Math.random() * 200 - 100) + 'px)';
 });
